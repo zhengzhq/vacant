cfc567377a092dffcf12cf845e17d5c81bfa5224
zhengzhq <13596449637@139.com>
2014-02-06 11:15:35 +0800

    all

---
+++ vacant/src/main/java/vacant/listener/ContextListener.java
@@ -0,0 +1,30 @@
+package vacant.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.log4j.Logger;
+
+public class ContextListener implements ServletContextListener {
+
+    private static final Logger log = Logger.getLogger(ContextListener.class);
+    
+	public void contextInitialized(ServletContextEvent sce) {
+        if (log.isInfoEnabled()) {
+            log.info("Servlet Context initialized...");
+        }
+        ServletContext context = sce.getServletContext();
+        //设置当前WEB路径上下文
+        String contextPath = context.getContextPath();
+        context.setAttribute("contextPath", contextPath);
+
+	}
+
+	public void contextDestroyed(ServletContextEvent sce) {
+        if (log.isInfoEnabled()) {
+            log.info("Servlet Context destroyed...");
+        }
+	}
+
+}
