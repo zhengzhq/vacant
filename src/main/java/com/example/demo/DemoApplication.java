e2a19ac70c57f50d2fddd749dd69ecfdad23cf1d
zzq <zzq@10.37.129.2>
2019-04-05 07:58:42 +0800

    恢复到第一版

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	// 注释
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
