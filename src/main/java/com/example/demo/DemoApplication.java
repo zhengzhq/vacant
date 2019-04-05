e77d693873490398fe4d67e4b2f971eada04eaa8
zzq <zzq@10.37.129.2>
2019-04-05 08:20:28 +0800

    提交第一次修改

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	// 第一次修改
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
