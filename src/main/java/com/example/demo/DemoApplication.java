d437859584cc9840002a398cb4489490227492a8
zzq <zzq@10.37.129.2>
2019-04-05 17:06:50 +0800

    在b1分枝上第一次修改

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	// 第一次修改
+	// 在b1分枝上第一次修改
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
