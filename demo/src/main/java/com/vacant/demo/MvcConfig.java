a495d7321ce9b54f347e2951a16ab83f5208fca3
zzq <zzq@DESKTOP-NAVS4SL>
2019-04-07 07:28:08 +0800

    实现登录

---
+++ demo/src/main/java/com/vacant/demo/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.vacant.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+    	registry.addViewController("/").setViewName("index");
+    	registry.addViewController("/index").setViewName("index");
+    	registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/main").setViewName("main");
+        registry.addViewController("/hello").setViewName("hello");
+    }
+
+}
