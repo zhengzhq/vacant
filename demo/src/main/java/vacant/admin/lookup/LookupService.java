f9f7f6ef2efa424547810522aed3f0f7b25bd111
zhengzhq <zzqyb@139.com>
2020-01-03 15:17:58 +0800

    over

--- demo/src/main/java/vacant/admin/lookup/LookupService.java
+++ demo/src/main/java/vacant/admin/lookup/LookupService.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
